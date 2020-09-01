2110f38cc5540c8bcd37110c249a1aa6ae6f38a5
Ivan Berk <berkoivan@gmail.com>
2020-09-02 01:08:55 +0300

    added main table for mobile 320

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import MainTable from "../MainTable/MainTable";
 
 export const App = () => {
-  return <div>Start project</div>;
+  return <MainTable></MainTable>;
 };
 
 export default App;
--- src/components/MainTable/MainTable.jsx
+++ src/components/MainTable/MainTable.jsx
@@ -1,5 +1,92 @@
 import React from "react";
+import { list, list__item, list__itemRow } from "./MainTable.module.css";
 
-const MainTable = () => <div></div>;
+const MainTable = ({ children }) => {
+  return (
+    <section>
+      <ul className={list}>
+        <li className={list__item} style={{ borderColor: "#75c16e" }}>
+          <div className={list__itemRow}>
+            <p>Date</p>
+            <p>07.01.19</p>
+          </div>
+          <div className={list__itemRow}>
+            <p>Type</p>
+            <p>+</p>
+          </div>
+          <div className={list__itemRow}>
+            <p>Comments</p>
+            <p>Irregular Income</p>
+          </div>
+          <div className={list__itemRow}>
+            <p>Comments</p>
+            <p>Cash</p>
+          </div>
+          <div className={list__itemRow}>
+            <p>Amount, UAH</p>
+            <p style={{ color: "#75c16e" }}>1 000.00</p>
+          </div>
+          <div className={list__itemRow}>
+            <p>Balance After</p>
+            <p>14 870.00</p>
+          </div>
+        </li>
+        <li className={list__item} style={{ borderColor: "#e28b20" }}>
+          <div className={list__itemRow}>
+            <p>Date</p>
+            <p>07.01.19</p>
+          </div>
+          <div className={list__itemRow}>
+            <p>Type</p>
+            <p>+</p>
+          </div>
+          <div className={list__itemRow}>
+            <p>Comments</p>
+            <p>Food</p>
+          </div>
+          <div className={list__itemRow}>
+            <p>Comments</p>
+            <p>Coffee with Tim</p>
+          </div>
+          <div className={list__itemRow}>
+            <p>Amount, UAH</p>
+            <p style={{ color: "#ff6c00" }}>30.00</p>
+          </div>
+          <div className={list__itemRow}>
+            <p>Balance After</p>
+            <p>13 870.00</p>
+          </div>
+        </li>
+        <li className={list__item} style={{ borderColor: "#d25925" }}>
+          <div className={list__itemRow}>
+            <p>Date</p>
+            <p>07.01.19</p>
+          </div>
+          <div className={list__itemRow}>
+            <p>Type</p>
+            <p>+</p>
+          </div>
+          <div className={list__itemRow}>
+            <p>Comments</p>
+            <p>Car</p>
+          </div>
+          <div className={list__itemRow}>
+            <p>Comments</p>
+            <p>Oil</p>
+          </div>
+          <div className={list__itemRow}>
+            <p>Amount, UAH</p>
+            <p style={{ color: "#ff6c00" }}>1 000.00</p>
+          </div>
+          <div className={list__itemRow}>
+            <p>Balance After</p>
+            <p>13 970.00</p>
+          </div>
+        </li>
+      </ul>
+      {children}
+    </section>
+  );
+};
 
 export default MainTable;
