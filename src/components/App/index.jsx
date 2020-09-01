a9ba39452073a8517d71b58628b7e461013241ce
Oleg Zavhorodnii <oleggenius95gmail.com>
2020-09-01 23:48:21 +0300

    added css variables and local fonts

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import styles from "./css.module.css";
 
 export const App = () => {
-  return <div className={styles.div}>Start project</div>;
+  return <div>Start project</div>;
 };
 
 export default App;
