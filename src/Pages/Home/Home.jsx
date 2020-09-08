1a9a6aef8c2b95c32688c977f67ddaac7319e874
Ivan Berk <berkoivan@gmail.com>
2020-09-08 19:20:35 +0300

    conflict fixed

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+// Components
+import Header from "../../components/Header/Header";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import MainInfo from "../../components/MainInfo/MainInfo";
+import { mainWrapper } from "./Home.module.css";
+
+const Home = (props) => {
+  return (
+    <>
+      <Header {...props} />
+      <div className={mainWrapper}>
+        <Sidebar />
+        <MainInfo />
+      </div>
+    </>
+  );
+};
+
+export default Home;
