1f5e1924af1f36f68e2cf11d99ab4862548d3906
Ivan Berk <berkoivan@gmail.com>
2020-09-10 16:06:52 +0300

    added feature finance reducer

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,18 +4,17 @@ import Header from "../../components/Header/Header";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import MainInfo from "../../components/MainInfo/MainInfo";
 import { mainWrapper } from "./Home.module.css";
-import withAuth from '../../hoc/withAuth';
+import withAuth from "../../hoc/withAuth";
 import { load } from "../../services/localStorage";
-
 
 const Home = (props) => {
   useEffect(() => {
-    props.getFinance();
     const data = load("session");
     if (data) {
       props.userSession(data);
     }
-  },[]);
+    props.getFinance();
+  }, []);
   return (
     <>
       <Header {...props} />
