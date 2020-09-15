4581f3a5767a87760a03d0cb8df721b81db2aff0
Ivan Berk <berkoivan@gmail.com>
2020-09-15 11:05:37 +0300

    some changed Graph styles

--- src/components/Graph/Graph.jsx
+++ src/components/Graph/Graph.jsx
@@ -1,34 +1,34 @@
 import React from "react";
 import { VictoryPie } from "victory";
-import {
-  graphBox,
-} from "./Graph.module.css";
+import { graphBox } from "./Graph.module.css";
 
+const Graph = ({ filteredData }) => {
+  const data = filteredData.map(({ category, categoryAmount }) => ({
+    x: category,
+    y: categoryAmount,
+  }));
 
-const Graph = ({filteredData }) => {
+  const colorFromDate = filteredData.map(({ color }) => color);
 
-  const data = filteredData.map(({category, categoryAmount}) => ({x: category, y: categoryAmount}));
-
- const colorFromDate = filteredData.map(({color})=> (color));
- 
   return (
-    
-      <div className={graphBox}>
-        <svg viewBox="0 0 1400 1400">
-          <VictoryPie
-            standalone={false}
-            width={1400}
-            height={1400}
-            data={data}
-            labelRadius={280}
-            style={{
-              labels: { fontSize: 40, fill: "white" },
-              data: { stroke: "#fff", strokeWidth: 2 },
-            }}
-          colorScale = {colorFromDate}
-          />
-        </svg>
-      </div>
+    <div className={graphBox}>
+      <svg viewBox="0 0 1400 1400">
+        <VictoryPie
+          standalone={false}
+          width={1400}
+          height={1400}
+          data={data}
+          labelRadius={250}
+          labelPlacement={({ index }) => (index ? "parallel" : "vertical")}
+          labelPosition={"centroid"}
+          style={{
+            labels: { fill: "white", fontSize: 40 },
+            data: { stroke: "#fff", strokeWidth: 2 },
+          }}
+          colorScale={colorFromDate}
+        />
+      </svg>
+    </div>
   );
 };
 
