0eea0e8718de2590cf571913af0e71c04dfbb3cf
Vadim Ponomarov <remary300@gmail.com>
2020-09-23 14:10:42 +0300

    added styles fix and portals

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -4,26 +4,22 @@ import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Registartion from "../../Pages/Registration/Registration";
 import Login from "../../Pages/Login/Login";
 import Home from "../../containers/HomeContainer";
-import PrivateRoute from "../PrivateRoute/PrivateRoute";
 import { load } from "../../services/localStorage";
 
 export const App = (props) => {
   const isAuthorized = load("session");
   return (
-    <div>
-      <Router>
-        <PrivateRoute path="/" component={Home} />
-        {/* <Route
-          path="/"
-          render={() =>
-            isAuthorized ? <Redirect to="/home" /> : <Redirect to="/login" />
-          }
-        /> */}
-        <Route path="/login" exact component={Login} />
-        <Route path="/registration" exact component={Registartion} />
-        <Route path="/home" component={Home} />
-      </Router>
-    </div>
+    <Router>
+      <Route
+        path="/"
+        render={() =>
+          isAuthorized ? <Redirect to="/home" /> : <Redirect to="/login" />
+        }
+      />
+      <Route path="/login" exact component={Login} />
+      <Route path="/registration" exact component={Registartion} />
+      <Route path="/home" component={Home} />
+    </Router>
   );
 };
 
